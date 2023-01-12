3ae70b2349b48e024307d657639913c4f94b39bb
Onur Bugdayci <onur.bugdayci@juvo.be>
2023-01-12 01:06:27 +0100

    chapter 10 Retrieving data using HTTP - done

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -7,12 +8,14 @@ import { ProductService } from "./product.service";
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
 
   pageTitle: string = 'Product List!';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
 
   private _listFilter: string = ' ';
   get listFilter(): string {
@@ -30,6 +33,9 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) {
 
   }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
@@ -40,7 +46,13 @@ export class ProductListComponent implements OnInit {
     this.showImage = !this.showImage;
   }
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products
+      },
+      error: err => this.errorMessage = err
+    });
     this.filteredProducts = this.products;
   }
   onRatingClicked(message: string): void {
